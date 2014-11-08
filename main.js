97de141bde630f07b0ca16414cc271f191c5e695
Legoben <legoben1998@gmail.com>
2014-11-08 14:35:51 -0600

    Updated JS

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+function BA_Click(id){
+    //Send to server   
+}
+
+
 //TODO: Get from server
 var database = {
-    "echo.newtechnetwork.org":["div.title-row > h2 > a"]   
+    "echo.newtechnetwork.org":[{"pattern":"div.title-row > h2 > a", "id":"sfkns"}]   
 }
 
 var allevents = database[document.location.hostname]
-console.log(allevents, $(allevents));
+console.log(allevents);
+
+for(var i = 0; i < allevents.length; i++){
+    console.log("here")
+    $(allevents[i].pattern).each(function(){
+        $(this).attr("onclick","BA_Click('"+allevents[i].id+"')");   
+    });
+}
