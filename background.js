d84080d5c554ec619eabd55ca5b71c897e70085f
Evan Seils <evan@seils.cc>
2014-11-08 23:32:41 -0600

    Fixed Chrome Extension

--- background.js
+++ background.js
@@ -1,6 +1,23 @@
-myURL="about:blank"; //A default url just in case below code doesn't work
-chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){ //onUpdated should fire when the selected tab is changed or a link is clicked 
-    chrome.tabs.getSelected(null,function(tab){
-        myURL=tab.url;
+var myURL = "about:blank";
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    chrome.tabs.getSelected(null, function (tab) {
+        myURL = tab.url;
+
+        function httpGet(url) {
+            var xmlHttp = null;
+
+            xmlHttp = new XMLHttpRequest();
+            xmlHttp.open("GET", url, false);
+            xmlHttp.send(null);
+            r = xmlHttp.responseText;
+            return xmlHttp.responseText;
+        }
+        var r = httpGet('https://ba.ngrok.com/getcount?url=' + myURL);
+        chrome.browserAction.setBadgeBackgroundColor({
+            color: [255, 0, 0, 255]
+        });
+        chrome.browserAction.setBadgeText({
+            text: r + ''
+        });
     });
 });
