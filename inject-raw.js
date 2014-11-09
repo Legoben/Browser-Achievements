ce633ee16bf8931b3d68fb5817cc60fbd12f8617
Evan Seils <evan@seils.cc>
2014-11-08 19:55:57 -0600

    click events

--- inject-raw.js
+++ inject-raw.js
@@ -1,6 +1,32 @@
-function BA_Click(id){
-    //Send to server 
-    alert(id);
-    alert("test");
+function BA_Click(id, userid, url) {
+    $.ajax({
+        url: 'https://ba.ngrok.com/ca',
+        data: {
+            uid: userid,
+            aid: id,
+            url: document.location.hostname
+        },
+        xhr: function () {
+            var xhr = jQuery.ajaxSettings.xhr();
+            var setRequestHeader = xhr.setRequestHeader;
+            xhr.setRequestHeader = function (name, value) {
+                if (name == 'X-Requested-With') return;
+                setRequestHeader.call(this, name, value);
+            }
+            return xhr;
+        },
+        success: function (r) {
+            if(typeof r == 'string'){
+                r = JSON.parse(r);
+            }
+            if(r.error != undefined){
+                console.log('Already Got!');
+                return;
+            }else{
+                console.log(r);
+                console.log('Sorry, Such Win!');
+            }
+            
+        }
+    });
 }
-
